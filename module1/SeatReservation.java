225330c1fdfb3c065b99ab9d65b8340698a4ba6d
Sophia Thi <sophie.x.thi@outlook.com>
2024-08-24 23:31:32 -0500

    Created SeatReservation.java

---
+++ module1/SeatReservation.java
@@ -0,0 +1,18 @@
+public class SeatReservation {
+
+    //Private instance variables
+    private String flightDesignator;
+    private java.time.LocalDate flightDate;
+    private String firstName;
+    private String lastName;
+        
+
+
+
+        
+    public static void main(String[] args){
+
+
+    }
+
+}
