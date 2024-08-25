99f05e6de59c965db9b9b05874e481748c4df6fd
Sophia Thi <sophie.x.thi@outlook.com>
2024-08-25 03:46:20 -0500

    Added methods

--- module1/SeatReservation.java
+++ module1/SeatReservation.java
@@ -5,13 +5,50 @@ public class SeatReservation {
     private java.time.LocalDate flightDate;
     private String firstName;
     private String lastName;
-        
+
+
+    //Getters and Setters
+    public String getFlightDesignation() {
+        return flightDesignation
+    }    
+
+    public void setFlightDesignation(String fd) {
+        this.flightDesignator = fd;
+    }
+
+    public java.time.LocalDate getFlightDate() {
+        return flightDate;
+    }
+
+    public void setFlightDate(java.time.LocalDate date) {
+        this.flightDate = date;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String fn) {
+        this.firstName = fn;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String ln) {
+        this.lastName = ln;
+    }
+
+    public String toString() {
+        //Will have to add something to deal with null exceptions
+        return getFlightDesignation() + getFlightDate() + getFirstName() + getLastName();
+    }
 
 
 
         
     public static void main(String[] args){
-
 
     }
 
