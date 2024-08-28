a09ba619bc35eec2f9cb41369f246e43d1963f72
Sophia Thi <sophie.x.thi@outlook.com>
2024-08-27 19:37:39 -0500

    Modified setFlightDesignator

--- module1/SeatReservation.java
+++ module1/SeatReservation.java
@@ -9,43 +9,71 @@ public class SeatReservation {
 
     //Getters and Setters
     public String getFlightDesignation() {
-        return flightDesignation
+        return flightDesignator;
     }    
 
     public void setFlightDesignation(String fd) {
-        this.flightDesignator = fd;
+        //We need to count the length of the string
+        int count = 0;
+        if (fd != null) {
+            for (int i = 0; i < fd.length(); i++) {
+                if (fd.charAt(i) != ' ') {
+                    count++;
+                }
+            }
+        }
+
+        //Strings less than 4 or greater than 6 are not allowed
+        if (count <= 4 || count >= 6) {
+            throw new IllegalArgumentException("Invalid flight designation");
+        } else {
+            this.flightDesignator = fd;
+        }
     }
 
     public java.time.LocalDate getFlightDate() {
+
         return flightDate;
     }
 
     public void setFlightDate(java.time.LocalDate date) {
+
         this.flightDate = date;
     }
 
     public String getFirstName() {
+
         return firstName;
     }
 
     public void setFirstName(String fn) {
+
         this.firstName = fn;
     }
 
     public String getLastName() {
+
         return lastName;
     }
 
     public void setLastName(String ln) {
+
         this.lastName = ln;
     }
 
     public String toString() {
         //Will have to add something to deal with null exceptions
-        return getFlightDesignation() + getFlightDate() + getFirstName() + getLastName();
+        if(flightDesignator == null) {
+            flightDesignator = "";
+        }
+        if(firstName == null) {
+            firstName = "";
+        }
+        if (lastName == null) {
+            lastName = "";
+        }
+        return "Flight Designator: " + flightDesignator + "\n" + "Flight Date: " + flightDate + "\n" + "Passenger Name: " + firstName + " " + lastName;;
     }
-
-
 
         
     public static void main(String[] args){
