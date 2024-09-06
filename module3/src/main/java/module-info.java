3b8076a7cfd78104721a2dc144182a03fc2debeb
Sophia Thi <sophie.x.thi@outlook.com>
2024-09-05 19:51:30 -0500

    Created new module

---
+++ module3/src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module edu.au.cpsc.module3 {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens edu.au.cpsc.module3 to javafx.fxml;
+    exports edu.au.cpsc.module3;
+}
