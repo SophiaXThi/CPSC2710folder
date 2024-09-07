75ca54795707c023fba0506c304f6bb1d6086d39
Sophia Thi <sophie.x.thi@outlook.com>
2024-09-07 03:14:16 -0500

    Modify class

--- module3/src/main/java/Airport.java
+++ module3/src/main/java/Airport.java
@@ -1,10 +1,6 @@
-import com.sun.source.tree.StringTemplateTree;
-
 import java.io.*;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.Scanner;
 
 public class Airport {
 
